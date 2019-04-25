552c0353aa2b26b942cbdcef989ecfe58c7168ba
yoheikikuta <diracdiego@gmail.com>
2019-04-25 21:31:33 +0900

    Initial commit
    It can handle a simple HTTP GET request using Volley.

---
+++ app/src/main/java/io/github/yoheikikuta/arxivquickcheckerandsaver/MainActivity.kt
@@ -0,0 +1,37 @@
+package io.github.yoheikikuta.arxivquickcheckerandsaver
+
+import android.app.DownloadManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text)
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://www.google.com"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            com.android.volley.Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                textView.text = "Response is: ${response.substring(0, 500)}"
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest)
+    }
+
+}
