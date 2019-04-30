0854386bf97eebfba801120a25a00800e8538307
yoheikikuta <diracdiego@gmail.com>
2019-04-30 19:46:07 +0900

    Add activity where we can change pages by swiping
    At this stage this activity only displays identical information
    (just displaying "title" "creator" "description").

--- app/src/main/java/io/github/yoheikikuta/arxivquickcheckerandsaver/MainActivity.kt
+++ app/src/main/java/io/github/yoheikikuta/arxivquickcheckerandsaver/MainActivity.kt
@@ -1,10 +1,19 @@
 package io.github.yoheikikuta.arxivquickcheckerandsaver
 
 import android.app.DownloadManager
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.view.ViewPager
 import android.text.Html
 import android.util.Xml
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import com.github.kittinunf.fuel.httpGet
 import java.net.URL
@@ -23,7 +32,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        data class Item(val title: String?, val creator: String?, val description: String?)
 
         val ns: String? = null
 
@@ -37,15 +45,6 @@ class MainActivity : AppCompatActivity() {
                 parser.nextTag()
                 return readFeed(parser)
             }
-//            fun parse(inputStream: InputStream): List<*> {
-//                inputStream.use { inputStream ->
-//                    val parser: XmlPullParser = Xml.newPullParser()
-//                    parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false)
-//                    parser.setInput(inputStream, null)
-//                    parser.nextTag()
-//                    return readFeed(parser)
-//                }
-//            }
 
             @Throws(XmlPullParserException::class, IOException::class)
             fun readFeed(parser: XmlPullParser): List<Item> {
@@ -142,26 +141,6 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-//        @Throws(IOException::class)
-//        fun downloadUrl(urlString: String): InputStream? {
-//            val url = URL(urlString)
-//            return (url.openConnection() as? HttpURLConnection)?.run {
-//                readTimeout = 10000
-//                connectTimeout = 15000
-//                requestMethod = "GET"
-//                doInput = true
-//                // Starts the query
-//                connect()
-//                inputStream
-//            }
-//        }
-//
-//        val items: List<Item> = downloadUrl("http://export.arxiv.org/rss/cs.CV")?.use { stream ->
-//            // Instantiate the parser
-//            ArxivRSSXmlParser().parse(stream)
-//        } as List<Item>? ?: emptyList()
-
-
         val textViewTitle = findViewById<TextView>(R.id.title)
         val textViewCreator = findViewById<TextView>(R.id.creator)
         val textViewDescription = findViewById<TextView>(R.id.description)
@@ -174,12 +153,72 @@ class MainActivity : AppCompatActivity() {
                 is Result.Success -> {
                     val data = result.get()
                     val items: List<Item>? = ArxivRSSXmlParser().parse(data) as List<Item>?
-                    textViewTitle.text = items!![0].title
-                    textViewCreator.text = items!![0].creator
-                    textViewDescription.text = items!![0].description
+                    textViewTitle.text = items!![1].title
+                    textViewCreator.text = items!![1].creator
+                    textViewDescription.text = items!![1].description
+
+                    val secondIntent = Intent(this, ScreenSlidePagerActivity::class.java)
+                    startActivity(secondIntent)
                 }
             }
         }
+
     }
 
+}
+
+data class Item(val title: String?, val creator: String?, val description: String?)
+
+class ScreenSlidePageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_screen_slide_page, container, false)
+}
+
+//class ScreenSlidePagerActivity(items:List<Item>?) : FragmentActivity() {
+class ScreenSlidePagerActivity() : FragmentActivity() {
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private lateinit var mPager: ViewPager
+//    private val numItems: Int? = items?.size
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_slide)
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = findViewById(R.id.pager)
+
+        // The pager adapter, which provides the pages to the view pager widget.
+        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        mPager.adapter = pagerAdapter
+    }
+
+    override fun onBackPressed() {
+        if (mPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            mPager.currentItem = mPager.currentItem - 1
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+//        override fun getCount(): Int = numItems!!
+        override fun getCount(): Int = 5
+
+        override fun getItem(position: Int): Fragment = ScreenSlidePageFragment()
+    }
 }
