b1b20611805d134f05b6f8aa93b44979a94b42c4
yoheikikuta <diracdiego@gmail.com>
2019-05-04 15:21:26 +0900

    Remove onBackPressed override
    Usual expected behavior is to quit the app when the back key is pressed.

--- app/src/main/java/io/github/yoheikikuta/arxivquickcheckerandsaver/MainActivity.kt
+++ app/src/main/java/io/github/yoheikikuta/arxivquickcheckerandsaver/MainActivity.kt
@@ -45,17 +45,6 @@ class MainActivity : FragmentActivity() {
         }
     }
 
-    override fun onBackPressed() {
-        if (pager.currentItem == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed()
-        } else {
-            // Otherwise, select the previous step.
-            pager.currentItem = pager.currentItem - 1
-        }
-    }
-
     /**
      * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in sequence.
      */
